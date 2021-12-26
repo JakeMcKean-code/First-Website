843024ad57f68e953e13ae1a951f7d929df9c4f9
Jake McKean <jake.mckean12@gmail.com>
2021-12-26 18:32:58 +0000

    Created derivative page

---
+++ Derivative_page.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import sympy as sp
+
+def create_page_layout():
+    '''Function to create the layout of the page'''
+
+    eq_input = st.text_input("Enter equation you want to differentiate:")
+    var_input = st.text_input("Enter variable to differetiate with respect to:")
+
+    if(eq_input and var_input):
+        eq_sp = sp.sympify(eq_input)
+        var_sp = sp.sympify(var_input)
+
+        st.latex(eq_sp.diff(var_sp))
--- Main.py
+++ Main.py
@@ -8,7 +8,13 @@ class Multiplage:
         """Constructor that instantiates the object and creates the empty list of pages"""
         self.pages = []
 
-    def add_page(self, title, func):
+    def add_page(self, title, func) -> None:
         """Function to add pages to the website"""
 
         self.pages.append({"title": title, "function": func})
+
+    def run(self) -> None:
+
+        page = st.sidebar.selectbox("Choose page",self.pages,format_func=lambda page: page['title'])
+
+        page['function']()
