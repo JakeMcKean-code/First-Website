d4de598d7f3b4f6c865d09bbea39524a95b9bb23
Jake McKean <jake.mckean12@gmail.com>
2021-12-26 18:51:30 +0000

    Reformatted

--- Derivative_page.py
+++ Derivative_page.py
@@ -9,7 +9,9 @@ def create_page_layout():
     eq_input = st.text_input("Enter equation you want to differentiate:")
     var_input = st.text_input("Enter variable to differetiate with respect to:")
     if var_input.isnumeric() == False:
-        st.text("Cannot differentiate with respect to a number, please input a valid differentiation variable")
+        st.text(
+            "Cannot differentiate with respect to a number, please input a valid differentiation variable"
+        )
 
     if eq_input and var_input:
         eq_sp = sp.sympify(eq_input)
