c19c4243ded2cd362b92a63b2fa77b46bde2a99e
Jake McKean <jake.mckean12@gmail.com>
2021-12-28 20:10:33 +0000

    Check error function to clean up code

--- Derivative_page.py
+++ Derivative_page.py
@@ -10,6 +10,15 @@ Add button to output latex code for user
 
 '''
 
+def check_input_numeric(input):
+    if input.isnumeric():
+            st.error(
+                "Cannot differentiate with respect to a number, please input a valid integration variable"
+            )
+            return
+    else:
+        return
+
 
 def create_page_layout():
     """Function to create the layout of the page"""
@@ -23,27 +32,39 @@ def create_page_layout():
     var_input = st.text_input("Enter variable to differetiate with respect to:")
 
     if eq_input and var_input:
-        if var_input.isnumeric():
-            st.error(
-                "Cannot differentiate with respect to a number, please input a valid integration variable"
-            )
-        else:
+        check_input_numeric(var_input)
+        #if var_input.isnumeric():
+        #    st.error(
+        #        "Cannot differentiate with respect to a number, please input a valid integration variable"
+        #    )
+        #else:
+        try:
+            eq_sp = sp.sympify(eq_input)
+            var_sp = sp.sympify(var_input)
+
             try:
-                eq_sp = sp.sympify(eq_input)
-                var_sp = sp.sympify(var_input)
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.subheader("Inputted equation")
+                    st.latex(eq_sp)
+                with col2:
+                    st.subheader("Derivative")
+                    st.latex(eq_sp.diff(var_sp))
+                    # st.latex(var_sp)
 
-                try:
-                    col1, col2 = st.columns(2)
-                    with col1:
-                        st.subheader("Inputted equation")
-                        st.latex(eq_sp)
-                    with col2:
-                        st.subheader("Derivative")
-                        st.latex(eq_sp.diff(var_sp))
-                        # st.latex(var_sp)
+            except ValueError as e:
+                st.error("Invalid intgration variable")
 
-                except ValueError as e:
-                    st.error("Invalid intgration variable")
+        except SympifyError as e:
+            st.error("Cannot parse equation or integration variable")
 
-            except SympifyError as e:
-                st.error("Cannot parse equation or integration variable")
+
+def check_for_sub():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.text_input("Input lower limit")
+    
+    with col2:
+        st.text_input("Input upper limit")
+    
+    
