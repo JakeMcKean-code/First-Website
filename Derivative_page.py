b67a03c668ffa057906324c0046a11f354b1d800
Jake McKean <jake.mckean12@gmail.com>
2021-12-28 20:20:45 +0000

    Added check variable functions to clean up main function

--- Derivative_page.py
+++ Derivative_page.py
@@ -15,9 +15,26 @@ def check_input_numeric(input):
             st.error(
                 "Cannot differentiate with respect to a number, please input a valid integration variable"
             )
-            return
+            return True
     else:
-        return
+        return False
+
+def check_input_parse(input):
+    try:
+        sp.sympify(input)
+        return True
+    except SympifyError as e:
+        st.error("Cannot parse equation or integration variable")
+        return False
+
+def check_variable(equation, variable):
+    try:
+        equation.diff(variable)
+        return True
+    except ValueError as e:
+        st.error("Invalid intgration variable")
+        return False
+
 
 
 def create_page_layout():
@@ -33,30 +50,23 @@ def create_page_layout():
 
     if eq_input and var_input:
         check_input_numeric(var_input)
-        #if var_input.isnumeric():
-        #    st.error(
-        #        "Cannot differentiate with respect to a number, please input a valid integration variable"
-        #    )
-        #else:
-        try:
+        
+        if(check_input_parse(eq_input) and check_input_parse(var_input)):
             eq_sp = sp.sympify(eq_input)
             var_sp = sp.sympify(var_input)
 
-            try:
-                col1, col2 = st.columns(2)
-                with col1:
-                    st.subheader("Inputted equation")
-                    st.latex(eq_sp)
-                with col2:
-                    st.subheader("Derivative")
+            col1, col2 = st.columns(2)
+            with col1:
+                st.subheader("Inputted equation")
+                st.latex(eq_sp)
+            with col2:
+                st.subheader("Derivative")
+                if(check_variable(eq_sp,var_sp)):
                     st.latex(eq_sp.diff(var_sp))
-                    # st.latex(var_sp)
+               
 
-            except ValueError as e:
-                st.error("Invalid intgration variable")
-
-        except SympifyError as e:
-            st.error("Cannot parse equation or integration variable")
+           
+    
 
 
 def check_for_sub():
