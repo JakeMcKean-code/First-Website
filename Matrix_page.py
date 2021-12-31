c44714d8baaee8c3a5f662142b60c436052fdd1e
Jake McKean <jake.mckean12@gmail.com>
2021-12-31 00:25:56 +0000

    Added to-do for two new pages

---
+++ Matrix_page.py
@@ -0,0 +1,6 @@
+"""
+Matrix page, add checkbox for:
+    General arithmatic
+    Eigenvalues + eigenvectors
+    Inverses
+"""
---
+++ Solve_equation_page.py
@@ -0,0 +1,12 @@
+"""
+
+Add column to tick to decide what kind of equation to solve for:
+    linear
+    Quadratic
+    Cubic
+    1st order ODE
+    2nd order ODE
+    Coupled Eqns
+    Eigenvalue eqn
+    
+"""
